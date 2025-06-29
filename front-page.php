5c2635832b8ad8d6be5cc70c732cfe8af5c39aa4
Ehtasham Muzaffar <ehtashammuzaffar@gmail.com>
2025-06-29 19:14:28 +0500

    homepage herosection completed

--- front-page.php
+++ front-page.php
@@ -1,16 +1,27 @@
-
-<?php 
+<?php
 get_header();
 ?>
 <section class="container_hero" id="hero_container">
         <div class="hero_col_1">
+                <div class="top_box">
+                <h1 class="main_heading">Ready To Go</h1>
+                <h2>Smart and efficient websites</h2>
+                <p class="h_paragraph">We specialize in building high-quality, template-based websites that save you time, energy, and money without sacrificing design or functionality.</p>
+                <div class="hero_buttons">
+                        <button class="h_left_btn button">Our Websites</button>
+                        <button class="h_right_btn button">Talk to us</button>
+                </div>
+                </div>
+                <div class="bottom_box">
+                          <img src="<?php echo get_template_directory_uri(); ?>/asstes/img/arrowdown.png" />
+                        <h5 class="hero_cta">Affordable options</h5>
+                </div>
 
-        </div>
-        <div class="hero_col_2">
-            <img src="<?php echo get_template_directory_uri(); ?>/asstes/img/hcol2.png" />
+         </div>
+          <div class="hero_col_2">
+                <img src="<?php echo get_template_directory_uri(); ?>/asstes/img/hcol2.png" />
         </div>
 </section>
-
 <?php
 get_footer();
 ?>
